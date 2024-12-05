2d352cccaa5a40f66d278d2a19a8f1b6d2a5237b
SidW111 <111293278+SidW111@users.noreply.github.com>
2024-12-05 23:01:32 +0530

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import '../App.css'
+
+const Navbar = () => {
+  return <div>
+  <nav className='container'>
+      <div className='logo'>
+        <img src="/images/brand_logo.png"/>
+        </div>
+
+      <ul>
+        <li href="#">MENU</li>
+        <li href="#">LOCATION</li>
+        <li href="#">ABOUT</li>
+        <li href="#">CONTACT</li>
+      </ul>
+
+      <button>Login</button>
+    </nav>
+    </div> 
+}
+
+export default Navbar
